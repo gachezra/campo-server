2090f22e89ae6b46b0627d86a6b0dde66975dffa
Ezra Kipkoech <71008423+gachezra@users.noreply.github.com>
2025-04-26 16:02:44 +0300

    Update Review.js

--- models/Review.js
+++ models/Review.js
@@ -1,24 +1,25 @@
 const mongoose = require('mongoose');
 
 const ReviewSchema = new mongoose.Schema({
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  university: { type: mongoose.Schema.Types.ObjectId, ref: 'University' },
-  branch: { type: mongoose.Schema.Types.ObjectId, ref: 'Branch' },
-  overall_rating: { "type": Number, required: true },
-  academic_rating: { "type": Number },
-  facilities_rating: { "type": Number },
-  social_life_rating: { "type": Number },
-  career_prospects_rating: { "type": Number },
-  cost_of_living: { "type": Number },
-  comment: { type: String },
-  responses: {
-    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    response: { type: String }
-  },
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  university: { type: mongoose.Schema.Types.ObjectId, ref: 'University', required: true },
+  branch: { type: mongoose.Schema.Types.ObjectId, ref: 'Branch', required: true },
+  overall_rating: { type: Number, required: true, min: 1, max: 10 },
+  academic_rating: { type: Number, required: true, min: 1, max: 10 },
+  facilities_rating: { type: Number, required: true, min: 1, max: 10 },
+  social_life_rating: { type: Number, required: true, min: 1, max: 10 },
+  career_prospects_rating: { type: Number, required: true, min: 1, max: 10 },
+  cost_of_living: { type: Number, required: true, min: 1 },
+  comment: { type: String, required: true },
+  responses: [{
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    response: { type: String, required: true },
+    date: { type: Date, default: Date.now }
+  }],
   date: { type: Date, default: Date.now }
 });
 
-// Create a unique compound index on user_id and university_id
-ReviewSchema.index({ user_id: 1, university_id: 1, branch: 1  }, { unique: true });
+// Create a unique compound index on user_id, university, and branch
+ReviewSchema.index({ user_id: 1, university: 1, branch: 1 }, { unique: true });
 
 module.exports = mongoose.model('Review', ReviewSchema);
